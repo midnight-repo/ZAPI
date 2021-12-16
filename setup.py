bc5eac3c243cf0114af3deec9eae0ad3465e9f6e
midnight_repo <midnight_repo@protonmail.com>
2021-12-16 00:42:19 +0000

    update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import os
 
 
 setup(name='zapy',
-      version='0.0.3',
+      version='0.0.4',
       description='OWASP ZAP Proxy API python package',
       author='midnight_repo',
       author_email='midnight_repo@protonmail.com',
