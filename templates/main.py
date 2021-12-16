3dd239f00e8521dd6e80e2057007742b9d338e0b
midnight_repo <midnight_repo@protonmail.com>
2021-12-16 17:03:04 +0000

    Added screenshots

--- templates/main.py
+++ templates/main.py
@@ -1,15 +1,19 @@
 import re
 import os
 from pathlib import Path
+from zapy.API import API
 
-{{IMPORTS_LIST}}
 
 def launch_ZAP():
-    os.system('zap.sh -daemon')
+    os.system('gnome-terminal -- zap.sh -daemon')
 
-ZAP_API_KEY = re.search('<key>(\w+)</key>', open(f'{Path.home()}/.ZAP/config.xml').read())[1]
-
-z = API('http://127.0.0.1:8081', ZAP_API_KEY)
-
+def read_key_from_FS():  # Works only on linux
+    return re.search('<key>(\w+)</key>', open(f'{Path.home()}/.ZAP/config.xml').read())[1]
 
 
+launch_ZAP()
+zap = API('http://127.0.0.1:8081', read_key_from_FS())
+
+spider_id = zap.spider.spiderActionScan(url='< TARGET URL >',
+                                        maxChildren=5,
+                                        recurse=True)
